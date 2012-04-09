0953fb9c63bc7d54a0070549323e096cd2d1c479
Benjamin <btleffler@gmail.com>
2012-04-09 23:27:57 +0000

    What's with all the spaces, man?

--- jquery.swapstyle.js
+++ jquery.swapstyle.js
@@ -4,37 +4,35 @@
  * Date: 09/14/11
  */
 
-( function ( $ ) {
+(function ($) {
     "use strict";
 
-    $.swapstyle = function ( original, replacement ) {
-        var s,          // Sheet index
-            r,          // Rule index
-            thisSheet,  // Cache each sheet as we loop through
-            thisNode,   // Node for link tags
-            href,       // Cache the href
-            cssRules,   // So we can store where the browser stores import rules
-            rule;       // Cache the rule
+    $.swapstyle = function (original, replacement) {
+        var s,         // Sheet index
+            r,         // Rule index
+            thisSheet, // Cache each sheet as we loop through
+            thisNode,  // Node for link tags
+            href,      // Cache the href
+            cssRules,  // So we can store where the browser stores import rules
+            rule;      // Cache the rule
 		
         // Loop through all the stylesheets
-        for ( s = 0; s < document.styleSheets.length; s++ ) {
+        for (s = 0; s < document.styleSheets.length; s++) {
             // The current stylesheet
-            thisSheet = document.styleSheets[ s ];
+            thisSheet = document.styleSheets[s];
 
             // If the href property isn't null, we should check the rule.
-            if (    thisSheet.href !== null &&
-                    typeof thisSheet.href !== "undefined"   ) {
-                if ( thisSheet.href.indexOf( original ) !== -1	) {				
+            if (thisSheet.href !== null && typeof thisSheet.href !== "undefined") {
+                if (thisSheet.href.indexOf(original) !== -1) {				
                     // Modify the link in question
                     thisNode = thisSheet.ownerNode;
 
                     if (thisSheet.owningElement) {
-                        //IE
-                        thisNode = thisSheet.owningElement;
+                        thisNode = thisSheet.owningElement; //IE
                     }
 
-                    href            = thisNode.href;
-                    thisNode.href   = href.replace( original, replacement );
+                    href          = thisNode.href;
+                    thisNode.href = href.replace(original, replacement);
                     
                     // We've already replaced the stylesheet, so we
                     // shouldn't haveto check any of it's rules.
@@ -45,45 +43,39 @@
             // Figure out where the browser keeps it's css rules
             cssRules = thisSheet.cssRules;
 
-            // IE
             if (thisSheet.imports) {
-                cssRules = thisSheet.imports;
+                cssRules = thisSheet.imports; // IE
             }
 
             // Go through the stylesheet's rules if there are any
-            if ( cssRules !== null ) {
-                for ( r = 0; r < cssRules.length; r++ ) {
+            if (cssRules !== null) {
+                for (r = 0; r < cssRules.length; r++) {
                     // The current rule
-                    rule = cssRules[ r ];
+                    rule = cssRules[r];
 
                     /* 
                      * If the rule has an href, it's an @import! We
                      * should check the href. I tried using 'instanceof,'
                      * but Opera had no idea how to handle that
                      *
-                    if (    rule instanceof CSSImportRule &&
-                            rule.href.indexOf(original) != -1   ) { */
+                    if (rule instanceof CSSImportRule && rule.href.indexOf(original) != -1) { */
 
-                    if (    rule.href !== null &&
-							typeof rule.href !== "undefined"    ) {
-                        if ( rule.href.indexOf( original ) !== -1 ) {
+                    if (rule.href !== null && typeof rule.href !== "undefined") {
+                        if (rule.href.indexOf(original) !== -1) {
                             // Insert the new rule at the end
-                            if ( thisSheet.insertRule ) {
-                                thisSheet.insertRule(
-                                    "@import url(" + replacement + ");",
-                                    cssRules.length
-                                );
+                            if (thisSheet.insertRule) {
+                                thisSheet.insertRule("@import url(" + replacement + ");", cssRules.length);
                             } else {
                                 // IE
-                                thisSheet.addImport( replacement, cssRules.length );
+                                thisSheet.addImport(replacement, cssRules.length);
                             }
 
                             // Delete the old one to get rid of clutter
-                            if ( thisSheet.deleteRule ) {
-                                thisSheet.deleteRule( r );
+                            if (thisSheet.deleteRule) {
+                                thisSheet.deleteRule(r);
                             } else {
                                 // IE
-                                thisSheet.removeImport( r );
+                                thisSheet.removeImport(r);
                             }
                         } // IF :rule.href.indexOf( original ) !== -1
                     } // IF: rule.href !== null && typeof rule.href !== "undefined"
@@ -91,4 +83,4 @@
             } // IF: typeof cssRules !== "undefined"
         } // FOR: s = 0; s < document.styleSheets.length; s++
     };
-} )( jQuery );
+})(jQuery);
